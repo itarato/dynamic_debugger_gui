d1d9d125fb283a48bee24298256b2d2be20f5fad
Peter Arato <it.arato@gmail.com>
2019-12-30 01:42:36 -0500

    Init.

---
+++ app/controllers/app.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"io/ioutil"
+
+	"github.com/revel/revel"
+	"gopkg.in/yaml.v2"
+)
+
+type Breakpoint struct {
+	Enabled    bool     `yaml:"enabled"`
+	Return     string   `yaml:"return"`
+	ReturnCode string   `yaml:"return_code"`
+	ReturnCall []string `yaml:"return_call"`
+	PreCall    []string `yaml:"pre_call"`
+	PostCall   []string `yaml:"post_call"`
+}
+
+type Config struct {
+	Breakpoints map[string]Breakpoint `yaml:"breakpoints"`
+}
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	yaml_file, err := ioutil.ReadFile("/home/itarato/.dynamic_debugger.config.yml")
+	if err != nil {
+		c.Log.Error("Cannot read config file.")
+	}
+
+	config := Config{}
+	err_yaml_load := yaml.Unmarshal(yaml_file, &config)
+
+	if err_yaml_load != nil {
+		c.Log.Error("YAML cannot be unamrshalled.")
+	}
+
+	c.ViewArgs["config"] = config
+
+	return c.Render()
+}
+
+func (c App) Update() revel.Result {
+	return c.Redirect(App.Index)
+}
