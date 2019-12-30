73a867a23f6212ae6e44a6afa40a91fdbc5ff9a8
Peter Arato <it.arato@gmail.com>
2019-12-30 17:41:01 -0500

    Add save.

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,23 @@
 package controllers
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/revel/revel"
 	"gopkg.in/yaml.v2"
 )
+
+const ConfigFileName = ".dynamic_debugger.config.yml"
+
+func ConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s/%s", homeDir, ConfigFileName), nil
+}
 
 type Breakpoint struct {
 	Enabled    bool     `yaml:"enabled"`
@@ -28,8 +40,23 @@ type App struct {
 	*revel.Controller
 }
 
+type ParamBreakpoint struct {
+	Enabled      string
+	ReturnOption string
+	Return       string
+	ReturnCode   string
+	ReturnCall   []string
+	PreCall      []string
+	PostCall     []string
+}
+
 func (c App) Index() revel.Result {
-	yaml_file, err := ioutil.ReadFile("/home/itarato/.dynamic_debugger.config.yml")
+	config_file_path, file_name_err := ConfigFilePath()
+	if file_name_err != nil {
+		panic("Cannot generate config file path")
+	}
+
+	yaml_file, err := ioutil.ReadFile(config_file_path)
 	if err != nil {
 		c.Log.Error("Cannot read config file.")
 	}
@@ -46,6 +73,52 @@ func (c App) Index() revel.Result {
 	return c.Render()
 }
 
+func UpdateConfigFromParam(breakpoints_raw map[string]ParamBreakpoint) error {
+	config := Config{make(map[string]Breakpoint)}
+
+	for name, param_breakpoint := range breakpoints_raw {
+		breakpoint := Breakpoint{}
+
+		breakpoint.Enabled = param_breakpoint.Enabled == "on"
+
+		if param_breakpoint.ReturnOption == "return" {
+			breakpoint.Return = param_breakpoint.Return
+		} else {
+			breakpoint.ReturnCode = param_breakpoint.ReturnCode
+		}
+
+		breakpoint.ReturnCall = param_breakpoint.ReturnCall
+		breakpoint.PreCall = param_breakpoint.PreCall
+		breakpoint.PostCall = param_breakpoint.PostCall
+
+		config.Breakpoints[name] = breakpoint
+	}
+
+	yaml_out, yaml_err := yaml.Marshal(&config)
+	if yaml_err != nil {
+		return yaml_err
+	}
+
+	config_file_path, file_name_err := ConfigFilePath()
+	if file_name_err != nil {
+		panic("Cannot generate config file path")
+	}
+	file_err := ioutil.WriteFile(config_file_path, yaml_out, 0664)
+	if file_err != nil {
+		return file_err
+	}
+
+	return nil
+}
+
 func (c App) Update() revel.Result {
+	var breakpoints map[string]ParamBreakpoint
+	c.Params.Bind(&breakpoints, "breakpoints")
+
+	update_err := UpdateConfigFromParam(breakpoints)
+	if update_err != nil {
+		c.Log.Error("Cannot update config file.")
+	}
+
 	return c.Redirect(App.Index)
 }
